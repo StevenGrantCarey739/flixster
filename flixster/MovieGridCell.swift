19e3e51587cf80331c8f8893a188f0fec3c8b534
Steven "Grant" Carey <careyst1@msu.edu>
2020-02-14 14:09:23 -0500

    Added superhero page to flixster app.

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieGridCell.swift
+//  
+//
+//  Created by Steven Carey on 2/12/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+}
